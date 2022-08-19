b3dfd0d05306209263672174a45428f183400624
Glitch (glitch-hello-sqlite) <none>
2022-08-19 11:52:28 +0000

    ⏰🚙 Checkpoint ./index.html:26026384/19 ./indexedDB.js:26026384/1940

--- indexedDB.js
+++ indexedDB.js
@@ -1,3 +1,8 @@
+if (!window.indexedDB) {
+    window.alert("Seu navegador não suporta uma versão estável do IndexedDB. Alguns recursos não estarão disponíveis.");
+}
+
+
 if (navigator.storage && navigator.storage.estimate) {
   const quota = navigator.storage.estimate();
   // quota.usage -> Número de bytes usados.
@@ -8,12 +13,58 @@ if (navigator.storage && navigator.storage.estimate) {
   console.log(`Você pode gravar até ${remaining} bytes mais.`);
 }
 
+const DadosClientes = [
+  { ssn: "444-44-4444", nome: "Bill", idade: 35, email: "bill@company.com" },
+  { ssn: "555-55-5555", nome: "Donna", idade: 32, email: "donna@home.org" }
+];
+
+var db;
 var connection =indexedDB.open('Clients',1)
+var request = indexedDB.open("Clients",1);
+
+request.onerror = function(event) {
+  alert("Você não habilitou minha web app para usar IndexedDB?!");
+};
+request.onsuccess = function(event) {
+  db = request.result;
+};
+
+
+request.onerror = function(event) {
+  // Tratar erros.
+};
+request.onupgradeneeded = function(event) {
+  var db = event.target.result;
+
+  // Cria um objectStore para conter a informação sobre nossos clientes. Nós vamos
+  // usar "ssn" como key path porque sabemos que é único;
+  var objectStore = db.createObjectStore("clientes", { keyPath: "ssn" });
+
+  // Cria um índice para buscar clientes pelo nome. Podemos ter nomes
+  // duplicados, então não podemos usar como índice único.
+  objectStore.createIndex("nome", "nome", { unique: false });
+
+  // Cria um índice para buscar clientes por email. Queremos ter certeza
+  // que não teremos 2 clientes com o mesmo e-mail;
+  objectStore.createIndex("email", "email", { unique: true });
+
+  // Usando transação oncomplete para afirmar que a criação do objectStore
+  // é terminada antes de adicionar algum dado nele.
+  objectStore.transaction.oncomplete = function(event) {
+    // Armazenando valores no novo objectStore.
+    var clientesObjectStore = db.transaction("clientes", "readwrite").objectStore("clientes");
+    for (var i in DadosClientes) {
+      clientesObjectStore.add(DadosClientes[i]);
+    }
+  }
+};
+
 
 connection.onsuccess = (e) => {
 var database = e.target.result;
-var transaction = database.transaction(['notes']); var objectStore = transaction.objectStore('notes');
-var index = objectStore.index('title'); var request = index.get("Chrome");
+var transaction = database.transaction(['Clients']); 
+var objectStore = transaction.objectStore('Clients');
+var index = objectStore.index('Client'); var request = index.get("Chrome");
 request.onsuccess = (e) = console.log(e.target.result);
 request.onerror= (e) => console.error(e.target.result);
 };
