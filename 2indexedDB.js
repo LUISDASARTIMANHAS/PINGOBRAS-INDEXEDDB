4b757045d7b4f5a536bb9810311a8bf41e66acad
Glitch (glitch-hello-sqlite) <none>
2022-08-25 23:41:30 +0000

    🌌👲 Checkpoint ./indexedDB.js:26026384/300 ./2indexedDB.js:26026384/4946 ./index.html:26026384/1

--- 2indexedDB.js
+++ 2indexedDB.js
@@ -4,11 +4,8 @@ if (!window.indexedDB) {
     window.alert("Seu navegador não suporta uma versão estável do IndexedDB. Alguns recursos não estarão disponíveis.");
 }
 
+
 var db;
-var request = indexedDB.open("DBteste");
-request.onerror = function(event) {
-  alert("Você não habilitou minha web app para usar IndexedDB?!");
-};
 const nomeAlert = prompt("Insira seu nome global",)
 stats.innerHTML = db
 request.onsuccess = function(event) {
@@ -22,6 +19,7 @@ request.onupgradeneeded = function(event) {
   // cria um objectStore para esse banco
   var objectStore = db.createObjectStore("nome", { keyPath: "minhaChave" });
 };
+
 // Isso é o que os dados de nossos clientes será.
 const DadosClientes = [
   { ssn: "444-44-4444", nome: "Bill", idade: 35, },
@@ -30,17 +28,9 @@ const DadosClientes = [
 ];
 const dbName = "clientes";
 
-var request = indexedDB.open(dbName, 2);
-
-request.onerror = function(event) {
-  // Tratar erros.
-};
-request.onupgradeneeded = function(event) {
-  var db = event.target.result;
-
   // Cria um objectStore para conter a informação sobre nossos clientes. Nós vamos
   // usar "ssn" como key path porque sabemos que é único;
-  var objectStore = db.createObjectStore("clientes", { keyPath: "ssn" });
+  var objectStore = db.createObjectStore("Storage", { keyPath: "ssn" });
 
   // Cria um índice para buscar clientes pelo nome. Podemos ter nomes
   // duplicados, então não podemos usar como índice único.
@@ -51,16 +41,11 @@ request.onupgradeneeded = function(event) {
   // é terminada antes de adicionar algum dado nele.
   objectStore.transaction.oncomplete = function(event) {
     // Armazenando valores no novo objectStore.
-    var clientesObjectStore = db.transaction("clientes", "readwrite").objectStore("clientes");
+    var StorageObjectStore = db.transaction("Storage", "readwrite").objectStore("clientes");
     for (var i in DadosClientes) {
-      clientesObjectStore.add(DadosClientes[i]);
+      StorageObjectStore.add(DadosClientes[i]);
     }
   }
-};
-// Abrindo o indexedDB.
-var request = indexedDB.open(dbName, 3);
-
-request.onupgradeneeded = function (event) {
 
     var db = event.target.result;
 
@@ -74,8 +59,8 @@ request.onupgradeneeded = function (event) {
     for (var i in DadosClientes) {
         objStore.add(DadosClientes[i].nome);
     }
-}
-var transaction = db.transaction(["clientes"], "readwrite");
+
+var transaction = db.transaction(["Storage"], "readwrite");
 // Nota: Implementações mais antigas usam uma versão IDBTransaction.READ_WRITE antiga em vez de "readwrite".
 // Então, para suportar versões antigas, escreva:
 // var transaction = db.transaction(["clientes"], IDBTransaction.READ_WRITE);
@@ -88,7 +73,7 @@ transaction.onerror = function(event) {
   // Não esquecer de tratar os erros!
 };
 
-var objectStore = transaction.objectStore("clientes");
+var objectStore = transaction.objectStore("Storage");
 for (var i in DadosClientes) {
   var request = objectStore.add(DadosClientes[i]);
   request.onsuccess = function(event) {
